8b779d190a09ad786d1468271a662c6a8e2b8bbf
Aliya Khabirzyanova <31696065+Khabirzyanova@users.noreply.github.com>
2023-10-15 21:31:13 +0300

    Update createdb.sql

--- hw01/createdb.sql
+++ hw01/createdb.sql
@@ -6,74 +6,58 @@ CREATE TABLE manufacturers (
 COPY manufacturers(
   "manufacturer_id"	SERIAL PRIMARY KEY,
   "manufacturer_name"	VARCHART(100) NOT NULL
-) FROM '/var/lib/postgresql/data/data.csv' DELIMITER ';' CSV HEADER;
+) FROM '/var/lib/postgresql/data/manufacturers.csv' DELIMITER ';' CSV HEADER;
 
-
-CREATE TABLE public.dataset (
-  "fixed acidity"           NUMERIC(18,4),
-  "volatile acidity"        NUMERIC(18,4),
-  "citric acid"             NUMERIC(18,4),
-  "residual sugar"          NUMERIC(18,4),
-  "chlorides"               NUMERIC(18,4),
-  "free sulfur dioxide"     NUMERIC(18,4),
-  "total sulfur dioxide"    NUMERIC(18,4),
-  "density"                 NUMERIC(18,4),
-  "pH"                      NUMERIC(18,4),
-  "sulphates"               NUMERIC(18,4),
-  "alcohol"                 NUMERIC(18,4),
-  "quality"                 INT
+CREATE TABLE categories (
+  "category_id"		SERIAL PRIMARY KEY,
+  "category_name"	VARCHART(100) NOT NULL
 );
 
-COPY public.dataset(
-  "fixed acidity",
-  "volatile acidity",
-  "citric acid",
-  "residual sugar",
-  "chlorides","free sulfur dioxide",
-  "total sulfur dioxide",
-  "density",
-  "pH",
-  "sulphates",
-  "alcohol",
-  "quality"
-) FROM '/var/lib/postgresql/data/data.csv' DELIMITER ';' CSV HEADER;
-\
-COPY manufacturers(\
- 	"manufacturer_id"	SERIAL PRIMARY KEY,\
- 	"manufacturer_name"	VARCHART(100) NOT NULL\
-) FROM '/var/lib/postgresql/data/data.csv' DELIMITER ';' CSV HEADER;\
-\
-\
-CREATE TABLE categories (\
- 	"category_id"	SERIAL PRIMARY KEY,\
- 	"category_name"	VARCHART(100) NOT NULL\
-);\
-\
-CREATE TABLE products (\
- 	\'93category_id\'94	BIGINT UNSIGNED,\
- 	"manufacturer_id"	BIGINT UNSIGNED,\
-	"product_id"	SERIAL PRIMARY KEY,\
-	 "product_name"	VARCHART(255) NOT NULL\
-);\
-\
-CREATE TABLE stores (\
-	store_id	SERIAL PRIMARY KEY,\
-	store_name	VARCHART(255) NOT NULL\
-);\
-\
-CREATE TABLE customers (\
-	customer_id	SERIAL PRIMARY KEY,\
-	customer_fname	VARCHART(100) NOT NULL,\
-	customer_lname	VARCHART(100) NOT NULL\
-);\
-\
-CREATE TABLE price_change (\
-	product_id		BIGINT UNSIGNED NOT NULL PRIMARY KEY,\
-	price_change_ts TIMESTAMP NOT NULL,\
-	new_price NUMERIC(9, 2) NOT NULL\
-);\
-\
-CREATE TABLE deliveries (\
+COPY categories(
+  "category_id"		SERIAL PRIMARY KEY,
+  "category_name"	VARCHART(100) NOT NULL
+) FROM '/var/lib/postgresql/data/categories.csv' DELIMITER ';' CSV HEADER;
+
+CREATE TABLE products (
+  "category_id"		BIGINT UNSIGNED,
+  "manufacturer_id"	BIGINT UNSIGNED,
+  "product_id"		SERIAL PRIMARY KEY,
+  "product_name"	VARCHART(255) NOT NULL
+);
+
+COPY products (
+  "category_id"		BIGINT UNSIGNED,
+  "manufacturer_id"	BIGINT UNSIGNED,
+  "product_id"		SERIAL PRIMARY KEY,
+  "product_name"	VARCHART(255) NOT NULL
+) FROM '/var/lib/postgresql/data/products.csv' DELIMITER ';' CSV HEADER;
+
+CREATE TABLE stores (
+  "store_id"		SERIAL PRIMARY KEY,
+  "store_name"		VARCHART(255) NOT NULL
+);
+
+COPY stores (
+  "category_id"		BIGINT UNSIGNED,
+  "manufacturer_id"	BIGINT UNSIGNED,
+  "product_id"		SERIAL PRIMARY KEY,
+  "product_name"	VARCHART(255) NOT NULL
+) FROM '/var/lib/postgresql/data/products.csv' DELIMITER ';' CSV HEADER;
+
+
+CREATE TABLE customers (
+  "customer_id"		SERIAL PRIMARY KEY,
+  "customer_fname"	VARCHART(100) NOT NULL,
+  "customer_lname"	VARCHART(100) NOT NULL
+);
+
+CREATE TABLE price_change (
+  "product_id"		BIGINT UNSIGNED NOT NULL PRIMARY KEY,
+  "price_change_ts"	TIMESTAMP NOT NULL,
+  "new_price"		NUMERIC(9, 2) NOT NULL
+);
+
+CREATE TABLE deliveries (
 	store_id	BIGINT UNSIGNED,\
 	product_id		BIGINT UNSIGNED NOT NULL,\
 	delivery_date		DATE NOT NULL,\
@@ -89,55 +73,5 @@ CREATE TABLE purchases (\
 \
 CREATE TABLE purchase_items (\
 );\
-\
-\
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
-\cf0 \
-CREATE TABLE public.dataset (\
-  "fixed acidity"           NUMERIC(18,4),\
-  "volatile acidity"        NUMERIC(18,4),\
-  "citric acid"             NUMERIC(18,4),\
-  "residual sugar"          NUMERIC(18,4),\
-  "chlorides"               NUMERIC(18,4),\
-  "free sulfur dioxide"     NUMERIC(18,4),\
-  "total sulfur dioxide"    NUMERIC(18,4),\
-  "density"                 NUMERIC(18,4),\
-  "pH"                      NUMERIC(18,4),\
-  "sulphates"               NUMERIC(18,4),\
-  "alcohol"                 NUMERIC(18,4),\
-  ""                 INT\
-);\
-\
-COPY public.dataset(\
-  "fixed acidity",\
-  "volatile acidity",\
-  "citric acid",\
-  "residual sugar",\
-  "chlorides","free sulfur dioxide",\
-  "total sulfur dioxide",\
-  "density",\
-  "pH",\
-  "sulphates",\
-  "alcohol",\
-  "quality"\
-) FROM '/var/lib/postgresql/data/data.csv' DELIMITER ';' CSV HEADER;\
-\
-CREATE TABLE public.models (\
-  "modelName"       TEXT PRIMARY KEY,\
-  "modelType"       TEXT NOT NULL,\
-  "modelParams"     TEXT NOT NULL,\
-  "isTrained"       BOOLEAN NOT NULL DEFAULT False,\
-  "trainAccuracy"   NUMERIC(21,20),\
-  "testAccuracy"    NUMERIC(21,20),\
-  "weights"         BYTEA,\
-  "modifyDate"      TIMESTAMP NOT NULL DEFAULT now()\
-);\
-\
-\
-\
-\
-\
-\pard\pardeftab720\partightenfactor0
 
-\f1\fs26 \cf2 \cb3 \expnd0\expndtw0\kerning0
-create table tablename (id serial primary key);}
+
